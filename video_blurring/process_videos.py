5f53dd08c6f35bf6696073b42f2e10c07b496a37
arielszabo <arielszabo@gmail.com>
2019-11-08 12:11:20 +0200

    re-arange

--- video_blurring/process_videos.py
+++ video_blurring/process_videos.py
@@ -1,9 +1,7 @@
 import os
 import yaml
-import cv2
 import pydarknet
-from tqdm import tqdm
-from detection_utils import get_video_output_full_path, find_bounds_in_image, get_video_params, add_blur
+from detection import blur_the_video, find_all
 
 
 CLASS_LABELS = ['car', 'person', 'motorbike', 'truck', 'bus']
@@ -50,91 +48,29 @@ def run(input_video_path, start_frame, end_frame, output_video_path):
                    end_frame=end_frame)
 
 
-def find_all(video_path, darknet_model,
-             class_labels, start_frame=0,
-             end_frame=None):
-    video_capture = cv2.VideoCapture(video_path)
+def get_video_name_and_extension(video_name):
+    """
+    Split the video name  todo: explain this better
+    """
+    reverse_video_name = video_name[::-1]
 
-    if end_frame is None:
-        end_frame = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
-
-    with tqdm(total=end_frame-start_frame, desc="Detect and find") as progress_bar:
-        frame_counter = 0
-        frames_bounds = []
-        while video_capture.isOpened():
-            ret, frame = video_capture.read()
-            if ret:
-                if end_frame > frame_counter > start_frame:
-
-                    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # todo: is this a must ?
-                    frame_results_bounds = find_bounds_in_image(image_frame=frame,
-                                                                darknet_model=darknet_model,
-                                                                class_label=class_labels)
-                    frames_bounds.append(frame_results_bounds)
-                    progress_bar.update(1)
-                frame_counter += 1
+    reversed_ending, reversed_beginning = reverse_video_name.split(".", maxsplit=1)
+    return reversed_beginning[::-1], reversed_ending[::-1]
 
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-            else:
-                break
+def get_video_output_full_path(video_name, video_start_frame, video_end_frame, output_folder_path):
+    core_video_name, video_extension = get_video_name_and_extension(video_name)
+    if video_start_frame is None:
+        video_start_frame = 0
 
-    # Release everything if job is finished
-    video_capture.release()
-    cv2.destroyAllWindows()
-    return frames_bounds
+    if video_end_frame is None:
+        frames_signature = str(video_start_frame)
+    else:
+        frames_signature = f"{video_start_frame}_{video_end_frame}"
+    output_video_name = f"{core_video_name}_{frames_signature}.{video_extension}"
+    output_full_path = os.path.join(output_folder_path, output_video_name)
+    return output_full_path
 
-
-def blur_the_video(video_path, output_path, frames_bounds, start_frame=0,
-                   end_frame=None):
-    cap = cv2.VideoCapture(video_path)
-
-    if cap.isOpened():
-        width, height, fps, fourcc = get_video_params(video_capture=cap)
-        print(width, height, fps, fourcc)
-
-        if end_frame is None:
-            end_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-
-        output = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-        with tqdm(total=end_frame - start_frame, desc="Blur found boundaries") as progress_bar:
-            frame_counter = 0
-            while cap.isOpened():
-                ret, frame = cap.read()
-                if ret:
-                    if end_frame > frame_counter > start_frame:
-                        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-                        for i in range(-AMOUNT_OF_NEAR_FRAMES_INCLUDE_BLUR, AMOUNT_OF_NEAR_FRAMES_INCLUDE_BLUR+1):
-                            real_index = frame_counter - start_frame
-                            if 0 <= real_index + i < len(frames_bounds):
-                                bounds = frames_bounds[real_index + i]
-
-                                frame = add_blur(frame,
-                                                  bounds,
-                                                  expand=False)
-
-                        new_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-                        output.write(new_frame)
-                        progress_bar.update(1)
-
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-
-                    frame_counter += 1
-
-
-                else:
-                    break
-
-        # Release everything if job is finished
-        output.release()
-
-    cap.release()
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
    main()
